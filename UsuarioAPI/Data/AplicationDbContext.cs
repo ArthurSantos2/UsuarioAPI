5f38ecfb62aa6a341f4e463db4a68d09e605717c
Arthur Santos <gaqtto@gmail.com>
2023-06-19 11:22:54 -0300

    Tratamento de exceção

--- UsuarioAPI/Data/AplicationDbContext.cs
+++ UsuarioAPI/Data/AplicationDbContext.cs
@@ -1,22 +1,36 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
+using System.Data.Common;
 using UsuarioAPI.Entities;
 
 namespace UsuarioAPI.Data
 {
     public class AplicationDbContext : DbContext
     {
-        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options) 
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
         {
             bool databaseExists = Database.CanConnect();
 
             if (!databaseExists)
             {
                 Database.Migrate();
+
+                bool migratedDatabaseExists = Database.CanConnect();
+
+                if (!migratedDatabaseExists)
+                {
+                    throw new DatabaseNotAccesibleException("O banco de dados não está acessível ou não existe.");
+                }
             }
-            
         }
 
         public DbSet<Users> Users { get; set; }
 
+        public class DatabaseNotAccesibleException : Exception
+        {
+            public DatabaseNotAccesibleException(string message) : base(message)
+            {
+            }
+        }
     }
 }
